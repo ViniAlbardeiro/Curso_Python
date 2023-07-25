46bae8b92f5eb9811c0b4ec0a54b74c6a9e1841b
ViniAlbardeiro <ender00247@gmail.com>
2023-07-25 11:12:06 -0300

    Atualização no código

--- Desafios - Python/Aula 17 - Listas/Desafio 081 - Extraindo Dados de uma Lista.py
+++ Desafios - Python/Aula 17 - Listas/Desafio 081 - Extraindo Dados de uma Lista.py
@@ -1,17 +1,18 @@
-list = []
+nums = []
 switch = '0'
 
 while switch not in 'Nn':
     n = int(input("\n Digite um número: "))
-    list.append(n)
+    nums.append(n)
     switch = '0'
     while switch not in 'SsNn':
         switch = str(input(" Deseja continuar? [S/N] "))
 
 print('\n', '-'*30)
-print(f'\n a)Foram digitados {len(list)} números.')
-print(f'\n b)Os números em ordem decrescente fica: {list.sort(reverse=True)}')
-if 5 in list:
+print(f'\n a)Foram digitados {len(nums)} números.')
+nums.sort(reverse=True)
+print(f'\n b)Os números em ordem decrescente fica: {nums}')
+if 5 in nums:
     print('\n c)O cinco faz parte da lista.')
 else:
     print('\n c)O cinco não faz parte da lista.')
